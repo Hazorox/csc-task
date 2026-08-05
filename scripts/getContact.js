c6d65436714225174ab6700c75f69406f3f8ab99
mazenDev <mazenfathy841@gmail.com>
2026-08-05 20:33:52 +0300

    styling cotact us

--- scripts/getContact.js
+++ scripts/getContact.js
@@ -4,14 +4,24 @@ const getContact = async () => {
   const data = await response.json();
   Object.keys(data).forEach((key) => {
     if (data[key].isMain) {
-      const div = document.createElement("div");
       const link = document.createElement("a");
+      const img = document.createElement("img");
+      const text = document.createElement("span");
+      text.innerText = data[key].name;
+      img.src = "../assets/whatsApp.png";
+      img.className = "w-25 absolute left-2 top-[50%] -translate-y-[50%]";
       link.target = "_blank";
-      link.innerText = "Send massege";
+      link.appendChild(img);
+      link.appendChild(text);
       const phone = data[key]["phone-number"].replace(/\D/g, "");
       link.href = `https://wa.me/${phone}`;
-      div.appendChild(link);
-      parent.appendChild(div);
+      link.className = `flex-1 
+      bg-white flex shadow-orange 
+      shadow-lg hover:shadow-sm 
+      transition-all duration-200 
+      active:scale-95 
+      justify-center items-center font-bold rounded-full select-none cursor-pointer relative`;
+      parent.appendChild(link);
     }
   });
 };
